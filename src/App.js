a496e7f7220712896a8891a265fa5a00fb338843
Suneel <paidisetty99@gmail.com>
2019-06-26 21:06:19 -0500

    react with routing of exisitng and new customer form

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import './App.css';
 import { NewCustomerForm } from "./NewCustomerForm.js";
 import { ExistingCustomerForm } from "./ExistingCustomerForm.js";
+import {Link} from "react-router-dom";
 
 function App() {
   return (
 <div className="App">
 <h1>QMS Price Exception Request System</h1>
         <h2>Customer Input</h2>
-        <button>NewCustomerForm</button>      
+        <Link to="/NewCustomerForm"><button>New Customer Form</button></Link> 
+        <Link to="/ExistingCustomerForm"><button>Existing Customer Form</button></Link>     
     </div>    
 
   );
